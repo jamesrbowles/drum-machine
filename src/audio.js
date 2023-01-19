07cbaff2e63d2e266d615b0f2610a911e5bdc877
James Bowles <jamesrbowles@gmail.com>
2023-01-19 09:45:05 +0100

    clean up

--- src/audio.js
+++ src/audio.js
@@ -1,13 +1,3 @@
-/* import Cev_H2 from "./assets/Cev_H2.mp3";
-import Dsc_Oh from "./assets/Dsc_Oh.mp3";
-import Heater1 from "./assets/Heater-1.mp3";
-import Heater2 from "./assets/Heater-2.mp3";
-import Heater3 from "./assets/Heater-3.mp3";
-import Heater4 from "./assets/Heater-4_1.mp3";
-import Heater6 from "./assets/Heater-6.mp3";
-import KicknHat from "./assets/Kick_n_Hat.mp3";
-import RP4Kick from "./assets/RP4_KICK_1.mp3"; */
-
 export const drumAudio = [
   {
     keyCode: 81,
